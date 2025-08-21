c79aa54084eabb61de73745d0f24b8f4570223fe
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2025-08-22 00:06:04 +0530

    Git loading animation

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,5 +1,17 @@
 async function pm2AppAction(appName, action){
     try {
+
+        
+        let elements = document.getElementsByClassName('icon-trigger-' + action);
+        if(elements && elements.length > 0){
+            Array.from(elements).forEach(element => {
+                element.classList.add('spin');                
+            });    
+        }
+        
+
+
+
         let response = await fetch(`/api/apps/${appName}/${action}`, { method: 'POST'})    
         
         if(!response.ok){
